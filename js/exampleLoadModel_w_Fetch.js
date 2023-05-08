f56343bcda22aea314654305c52dfee1ce9dd030
Yuni <tc1006@georgetown.edu>
2023-05-07 23:05:26 -0400

    0507 start to write

--- js/exampleLoadModel_w_Fetch.js
+++ js/exampleLoadModel_w_Fetch.js
@@ -2,13 +2,16 @@
 
 let ModelMaterialsArray = []; // an array of materials
 let ModelAttributeArray = []; // vertices, normals, textcoords, uv
-
+let TextureArray = [["Clouds_2K.png","Bump_2K.png","Dust_2K.png",'Mars_Diffuse_2K.png'],['base_color.jpg'],['Earth_Bump_2K.png','Earth_Clouds_2K.png','Earth_Diffuse_2K.png'],['Mercury_Bump_1K.png','Mercury_Diffuse_1K.png'],['Venus_Atmosphere_2K.png','Venus_Diffuse_1K.png']]; // store texture
 /**
  * Entry Point for HTML <body onload() >
  * Uses Asynchronous HTTP request to load 3D Mesh Model in JSON format
  * 
  */
 let camera;
+let load_model = 0;
+let translate_list = [[-1.0, -0.5, -6.0],[-8.0,-0.5,-6.0],[-11.0,-0.5,-2],[8.0,-0.5,-3.0],[-4.0, -0.5, -9.0]];
+let model_load_list = [];
 document.addEventListener("keydown", ProcessKeyPressedEvent, false )
 
 class Camera{
@@ -200,9 +203,19 @@ function myMain() {
 
     // uri is relative to directory containing HTML page
     camera = new Camera();
-    // loadExternalJSON('./model/crate.json');
-    loadExternalJSON('./model/Mars 2K.json');
+    // let model_num = 0;
+    loadExternalJSON('./model/Mars 2K.json',0);
+    loadExternalJSON('./model/Moon_2K.json',1);
+    loadExternalJSON('./model/Earth 2K.json',2);
+    loadExternalJSON('./model/Mercury 1K.json',3);
+    loadExternalJSON('./model/Venus_1K.json',4);
+
+
+
+
+
     
+
 
     
 
@@ -318,17 +331,35 @@ function setUpWebGL() {
 
   // Here's where we call the routine that builds all the
   // objects we'll be drawing.
-  const buffers = initBuffers(gl);
+  let buffers_list = [];
+  let idx_ = 0;
+  let total  = model_load_list.length
+  for (idx_ = 0;idx_< total;idx_++){
+    const buffers = initBuffers(gl,idx_);
+    buffers_list.push(buffers);
+  }
+  
+
 
   // Load texture
   // console.log('/texture/'+ ModelAttributeArray[0].textureFile);
-  const texture = loadTexture(gl, './texture/'+ 'Mars_Diffuse_2K.png');
+  let texture_list = [];
+  let idx = 0;
+  
+  for (idx = 0; idx<total;idx++){
+    let model = model_load_list[idx][0];
+    let index = model_load_list[idx][1];
+    console.log('./texture/'+ TextureArray[model][index]);
+    const texture = loadTexture(gl, './texture/'+ TextureArray[model][index]);
+    texture_list.push(texture);
+
+  }
   // Flip image pixels into the bottom-to-top order that WebGL expects.
   gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 
   // Draw the scene repeatedly
   function render(now) {
-    drawScene(gl, programInfo, buffers, texture,camera.cameraMatrix);
+    drawScene(gl, programInfo, buffers_list, texture_list,camera.cameraMatrix,now);
 
     requestAnimationFrame(render);
   }
@@ -342,7 +373,7 @@ function setUpWebGL() {
  * 
  * @param {JSON} obj2 3D Model in JSON Format
  */
-function createModelAttributeArray(obj2) {
+function createModelAttributeArray(obj2,model_num) {
     // obj.mesh[x] is an array of attributes
     // vertices, normals, texture coord, indices
 
@@ -382,6 +413,7 @@ function createModelAttributeArray(obj2) {
         // modelObj.textureFile = obj2.materials[modelObj.materialIndex].properties.find(x => x.key === '$tex.file').value;
         // push onto array
         ModelAttributeArray.push(modelObj);
+        model_load_list.push([model_num,idx]);
     }
 }
 /**
@@ -447,7 +479,7 @@ function createMaterialsArray(obj2){
  * 
  * @param {uri} url -- the uri for the 3D Model to load. File should be a JSON format
  */
-function loadExternalJSON(url) {
+function loadExternalJSON(url,model_num) {
     fetch(url)
         .then((resp) => {
             // if the fetch does not result in an network error
@@ -459,8 +491,13 @@ function loadExternalJSON(url) {
             // get a reference to JSON mesh model for debug or other purposes 
             obj = ModelInJson;
             createMaterialsArray(ModelInJson);
-            createModelAttributeArray(ModelInJson);
-            setUpWebGL();
+            createModelAttributeArray(ModelInJson,model_num);
+            if (load_model < 3){
+              load_model+=1;
+            }else{
+              setUpWebGL();
+            }
+
         })
         .catch(function (error) {
             // error retrieving resource put up alerts...
@@ -470,14 +507,14 @@ function loadExternalJSON(url) {
 }
 
 
-function initBuffers(gl) {
-    const positionBuffer = initPositionBuffer(gl);
+function initBuffers(gl,idx) {
+    const positionBuffer = initPositionBuffer(gl,idx);
   
-    const textureCoordBuffer = initTextureBuffer(gl);
+    const textureCoordBuffer = initTextureBuffer(gl,idx);
   
-    const indexBuffer = initIndexBuffer(gl);
+    const indexBuffer = initIndexBuffer(gl,idx);
   
-    const normalBuffer = initNormalBuffer(gl);
+    const normalBuffer = initNormalBuffer(gl,idx);
   
     return {
       position: positionBuffer,
@@ -487,27 +524,33 @@ function initBuffers(gl) {
     };
   }
   
-  function initPositionBuffer(gl) {
+  function initPositionBuffer(gl,idx) {
     // Create a buffer for the square's positions.
-    const positionBuffer = gl.createBuffer();
-  
-    // Select the positionBuffer as the one to apply buffer
-    // operations to from here out.
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  
-    const positions = ModelAttributeArray[0].vertices;
-  
-    // Now pass the list of positions into WebGL to build the
-    // shape. We do this by creating a Float32Array from the
-    // JavaScript array, then use it to fill the current buffer.
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-  
+    // let global_positionBuffer = [];
+    // let idx=0;
+    // for (idx = 0; idx < numMeshIndexs; idx++){
+
+    // }
+
+      const positionBuffer = gl.createBuffer();
+      
+      // Select the positionBuffer as the one to apply buffer
+      // operations to from here out.
+      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    
+      const positions = ModelAttributeArray[idx].vertices;
+    
+      // Now pass the list of positions into WebGL to build the
+      // shape. We do this by creating a Float32Array from the
+      // JavaScript array, then use it to fill the current buffer.
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    
     return positionBuffer;
   }
   
 
   
-  function initIndexBuffer(gl) {
+  function initIndexBuffer(gl,idx) {
     const indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
   
@@ -515,7 +558,7 @@ function initBuffers(gl) {
     // indices into the vertex array to specify each triangle's
     // position.
   
-    const indices = ModelAttributeArray[0].indices;
+    const indices = ModelAttributeArray[idx].indices;
   
     // Now send the element array to GL
   
@@ -528,11 +571,11 @@ function initBuffers(gl) {
     return indexBuffer;
   }
   
-  function initTextureBuffer(gl) {
+  function initTextureBuffer(gl,idx) {
     const textureCoordBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
   
-    const textureCoordinates = ModelAttributeArray[0].textCoord;
+    const textureCoordinates = ModelAttributeArray[idx].textCoord;
   
   
     gl.bufferData(
@@ -544,11 +587,11 @@ function initBuffers(gl) {
     return textureCoordBuffer;
   }
   
-  function initNormalBuffer(gl) {
+  function initNormalBuffer(gl,idx) {
     const normalBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
   
-    const vertexNormals = ModelAttributeArray[0].normals;
+    const vertexNormals = ModelAttributeArray[idx].normals;
   
     gl.bufferData(
       gl.ARRAY_BUFFER,
@@ -558,7 +601,9 @@ function initBuffers(gl) {
   
     return normalBuffer;
   }
-  function drawScene(gl, programInfo, buffers, texture, camera_view_matrix) {
+  function drawScene(gl, programInfo, buffers, texture, camera_view_matrix,now) {
+    now *= 0.0001;
+    
     gl.clearColor(0.0, 0.0, 0.0, 1.0); 
     gl.clearDepth(1.0); 
     gl.enable(gl.DEPTH_TEST); 
@@ -568,76 +613,93 @@ function initBuffers(gl) {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   
   
-  
-    const fieldOfView = (45 * Math.PI) / 180; 
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const zNear = 0.1;
-    const zFar = 100.0;
-    const projectionMatrix = mat4.create();
-  
-  
-    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
-  
-  
-    const modelViewMatrix = mat4.create();
-  
-  
-    mat4.translate(
-      modelViewMatrix, 
-      modelViewMatrix, 
-      [-1.0, -0.5, -6.0]
-    ); 
-  
+    let idx = 0;
+    let total = model_load_list.length
+    for (idx=0;idx<total;idx++){
+      // console.log(idx);
+      const fieldOfView = (45 * Math.PI) / 180; 
+      const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+      const zNear = 0.1;
+      const zFar = 100.0;
+      const projectionMatrix = mat4.create();
     
-    setPositionAttribute(gl, buffers, programInfo);
-  
-    setTextureAttribute(gl, buffers, programInfo);
-  
-  
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
-  
-    setNormalAttribute(gl, buffers, programInfo);
-  
-  
-    gl.useProgram(programInfo.program);
-  
-    gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix,false,projectionMatrix);
-    gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix,false,modelViewMatrix);
-    gl.uniformMatrix4fv(programInfo.uniformLocations.cameraViewMatrix,false,camera_view_matrix);
-    // var normalMatrix = mat4.create();
-    const normalMatrix = mat4.create();
-    mat4.invert(normalMatrix, modelViewMatrix);
-    mat4.transpose(normalMatrix, normalMatrix);
-  
-    gl.uniformMatrix4fv(
-      programInfo.uniformLocations.normalMatrix,
-      false,
-      normalMatrix
-    );
     
-    gl.uniform3fv(programInfo.uniformLocations.diffuseLightColor, ModelMaterialsArray[ModelAttributeArray[0].materialIndex].diffuse);
-    gl.uniform3fv(programInfo.uniformLocations.lightDirection, [-5.0, 1.0, 1.0]);
-    gl.uniform3fv(programInfo.uniformLocations.ambientLightColor, ModelMaterialsArray[ModelAttributeArray[0].materialIndex].ambient);
+      mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
     
-    gl.uniform3fv(programInfo.uniformLocations.specularLightColor, ModelMaterialsArray[ModelAttributeArray[0].materialIndex].specular);
-    gl.uniform1f(programInfo.uniformLocations.shininess, ModelMaterialsArray[ModelAttributeArray[0].materialIndex].shininess);
+    
+      let modelViewMatrix = mat4.create();
+    
+      let modelMatrix_s = mat4.create();
+      model_idx = model_load_list[idx][0];
+      mat4.translate(
+        modelMatrix_s, 
+        modelMatrix_s, 
+        [0.5, 2.0, -5.0]
+      ); 
+      mat4.rotate(modelMatrix_s,modelMatrix_s,now*2,[0.0,1.0,0.0]);
+      // console.log(translate_list[model_idx]);
+      mat4.translate(
+        modelViewMatrix, 
+        modelViewMatrix, 
+        translate_list[model_idx]
+        
+      ); 
+      mat4.multiply(modelViewMatrix,modelMatrix_s,modelViewMatrix);
+      mat4.rotate(modelViewMatrix, modelViewMatrix, now, [0.0, 1.0, 0.0]);
     
       
+      setPositionAttribute(gl, buffers[idx], programInfo);
+    
+      setTextureAttribute(gl, buffers[idx], programInfo);
+    
+    
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers[idx].indices);
+    
+      setNormalAttribute(gl, buffers[idx], programInfo);
+    
+    
+      gl.useProgram(programInfo.program);
+    
+      gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix,false,projectionMatrix);
+      gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix,false,modelViewMatrix);
+      gl.uniformMatrix4fv(programInfo.uniformLocations.cameraViewMatrix,false,camera_view_matrix);
+      // var normalMatrix = mat4.create();
+      const normalMatrix = mat4.create();
+      mat4.invert(normalMatrix, modelViewMatrix);
+      mat4.transpose(normalMatrix, normalMatrix);
+    
+      gl.uniformMatrix4fv(
+        programInfo.uniformLocations.normalMatrix,
+        false,
+        normalMatrix
+      );
       
-  
-    gl.activeTexture(gl.TEXTURE0);
-  
-  
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-  
-  
-    gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
-  
-    const vertexCount = ModelAttributeArray[0].indices.length;
-    // console.log(buffers.indices.length)
-    const type = gl.UNSIGNED_SHORT;
-    const offset = 0;
-    gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+      gl.uniform3fv(programInfo.uniformLocations.diffuseLightColor, ModelMaterialsArray[ModelAttributeArray[0].materialIndex].diffuse);
+      gl.uniform3fv(programInfo.uniformLocations.lightDirection, [0.5, 2.0, -5.0]);
+      // gl.uniform3fv(programInfo.uniformLocations.ambientLightColor, ModelMaterialsArray[ModelAttributeArray[0].materialIndex].ambient);
+      gl.uniform3fv(programInfo.uniformLocations.ambientLightColor, [0.5,0.5,0.5]);
+      
+      gl.uniform3fv(programInfo.uniformLocations.specularLightColor, ModelMaterialsArray[ModelAttributeArray[0].materialIndex].specular);
+      gl.uniform1f(programInfo.uniformLocations.shininess, ModelMaterialsArray[ModelAttributeArray[0].materialIndex].shininess);
+      
+        
+        
+    
+      gl.activeTexture(gl.TEXTURE0);
+    
+    
+      gl.bindTexture(gl.TEXTURE_2D, texture[idx]);
+    
+    
+      gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
+    
+      const vertexCount = ModelAttributeArray[idx].indices.length;
+      // console.log(buffers.indices.length)
+      const type = gl.UNSIGNED_SHORT;
+      const offset = 0;
+      gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+    }
+      
   }
   
   
